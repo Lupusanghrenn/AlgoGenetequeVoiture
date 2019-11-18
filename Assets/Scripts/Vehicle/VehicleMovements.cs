87507c33cd5be66886712609d25c5289a219b77c
Aloïs <marrocq.alois@laposte.net>
2019-11-18 11:47:43 +0100

    Minor

---
+++ Assets/Scripts/Vehicle/VehicleMovements.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class VehicleMovements : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    float speed = 5f;
+       
+    private void FixedUpdate()
+    {
+        
+    }
+}
