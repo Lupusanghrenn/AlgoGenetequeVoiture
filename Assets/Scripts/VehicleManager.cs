e9ed1acef936e15b65d773386d0bbc76c2812132
Aloïs <marrocq.alois@laposte.net>
2019-11-18 10:41:03 +0100

    Debug ray hit correctly

--- Assets/Scripts/VehicleManager.cs
+++ Assets/Scripts/VehicleManager.cs
@@ -6,6 +6,8 @@ public class VehicleManager : MonoBehaviour
     public float maxRange = 10.0f;
     public Transform Origin;
 
+    private bool obstacleHit = false;
+
     void Update()
     {
         //VehiclesMovement();
@@ -14,35 +16,46 @@ public class VehicleManager : MonoBehaviour
 
     public void DebugRaycast()
     {
+        obstacleHit = false;
         //right max range vector
         Vector3 vectorRight = Quaternion.AngleAxis(45, Vector3.up) * transform.forward;
 
         //left max range vector
         Vector3 vectorLeft = Quaternion.AngleAxis(-45, Vector3.up) * transform.forward;
-
-        //Debug rays no hit
-        Debug.DrawRay(Origin.position, vectorRight * maxRange, Color.red);
-        Debug.DrawRay(Origin.position, vectorLeft * maxRange, Color.red);
-        Debug.DrawRay(Origin.position, transform.forward * maxRange, Color.red);
-
+        
         //Debug rays hit
         RaycastHit hit;
         if (Physics.Raycast(Origin.position, transform.forward, out hit, maxRange))
         {
             Debug.DrawRay(Origin.position, transform.forward * hit.distance, Color.green);
             Debug.Log("Did Hit");
+            obstacleHit = true;
+        }
+        else
+        {
+            Debug.DrawRay(Origin.position, transform.forward * maxRange, Color.red);
         }
 
         if (Physics.Raycast(Origin.position, vectorRight, out hit, maxRange))
         {
             Debug.DrawRay(Origin.position, vectorRight * hit.distance, Color.green);
             Debug.Log("Did Hit");
+            obstacleHit = true;
+        }
+        else
+        {
+            Debug.DrawRay(Origin.position, vectorRight * maxRange, Color.red);
         }
 
         if (Physics.Raycast(Origin.position, vectorLeft, out hit, maxRange))
         {
             Debug.DrawRay(Origin.position, vectorLeft * hit.distance, Color.green);
             Debug.Log("Did Hit");
+            obstacleHit = true;
+        }
+        else
+        {
+            Debug.DrawRay(Origin.position, vectorLeft * maxRange, Color.red);
         }
     }
 
