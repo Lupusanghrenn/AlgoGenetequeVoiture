50d3fc5b05e26a8a33f41b77f6dc54d1ada6b826
Aloïs <marrocq.alois@laposte.net>
2019-11-18 14:30:14 +0100

    Car steers

--- Assets/Scripts/Vehicle/VehicleMovements.cs
+++ Assets/Scripts/Vehicle/VehicleMovements.cs
@@ -3,16 +3,19 @@
 public class VehicleMovements : MonoBehaviour
 {
     public Rigidbody rb;
-    [Range(0, 1)]
+    [Range(0, 100)]
     public float speed = 0.5f;
+    public float maxAngleTurn = 30;
 
     [Range(-1, 1)]
     public float steer;
        
     private void FixedUpdate()
     {
-        rb.MovePosition(transform.position + new Vector3(0, 0, speed));
+        rb.velocity = transform.forward * speed * Time.deltaTime;
 
-        
+        Vector3 eulerAngleVelocity = new Vector3(0, steer * maxAngleTurn, 0);
+        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
